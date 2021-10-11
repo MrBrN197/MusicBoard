70a56ffc33d356d461851087aa48f1b34c2fdec2
brianorora <brianorora@gmail.com>
2021-10-11 22:25:47 +0300

    Finish webpack configuration
    - Add css, sass configuration
    - Add postcss
    - Add webpack-dev-server live reloading
    - Add style.scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,35 @@
+/* eslint-disable */
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
   plugins: [
     new HTMLWebpackPlugin({
       template: './src/template.html',
     }),
   ],
-  output: {
-    path: path.resolve(__dirname, './dist'),
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    watchFiles: '**/*.src',
+    static: './dist',
   },
 };
