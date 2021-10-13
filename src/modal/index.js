e37c091554882f6dd4b9f3636bfab65dd5df89c0
brianorora <brianorora@gmail.com>
2021-10-14 01:20:11 +0300

    Display correct image on modal

--- src/modal/index.js
+++ src/modal/index.js
@@ -2,7 +2,7 @@ import './modal.scss';
 import API from '../api/functions.js';
 
 export default {
-  showModal: async ({ id }) => {
+  showModal: async ({ image, id }) => {
     const modal = document.createElement('div');
     modal.id = 'modal';
     document.body.appendChild(modal);
@@ -13,7 +13,7 @@ export default {
           <i class="fas fa-times"></i>
         </div>
         <div class="image">
-          <img src="https://images.unsplash.com/photo-1632207191677-8446985f9d65?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1161&q=80" alt="album-art">
+          <img src="${image}" alt="album-art">
         </div>
         <span class="heading">Space 3</span>
         <span class="sub-heading">Comments (2)</span>
