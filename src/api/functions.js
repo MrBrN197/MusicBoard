9fc9f8b25933953f8e88b1c1486662fe17cb61c1
brianorora <brianorora@gmail.com>
2021-10-13 23:48:38 +0300

    Create different data for each id

--- src/api/functions.js
+++ src/api/functions.js
@@ -1,47 +1,18 @@
 import APIFactory from './factory.js';
 import credentials from './credentials.js';
 
+import data from './data.js';
+
 const apiPlaceholder = APIFactory({ credentials, fetch });
 
 // NOTE: THIS are placeholders for the real API
 
 const fakeData = {
-  likes: [
-    {
-      item_id: '5Gso3d4CscCijv0lmajZWd',
-      likes: 0,
-    },
-  ],
-  all_comments: [
-    {
-      item_id: '5Gso3d4CscCijv0lmajZWd',
-      comments: [
-        {
-          creation_date: '2021-10-12',
-          username: 'Mrs Man',
-          comment: 'This is the first comment',
-        },
-        {
-          creation_date: '2021-10-13',
-          username: 'Mrs Girl',
-          comment: 'This is the second comment',
-        },
-      ],
-    },
-  ],
+  likes: data.items.map((item) => ({ item_id: item.id, likes: 0 })),
+  all_comments: data.items.map((item) => ({ item_id: item.id, comments: [] })),
 };
 
-apiPlaceholder.getNewReleases = async () => {
-  const object = {
-    id: '5Gso3d4CscCijv0lmajZWd',
-    album_name: 'Life of a DON',
-    artist: 'Don Toliver',
-    image: 'https://i.scdn.co/image/ab67616d0000b273ab738b25b86bf02f0346c53d',
-    likes: 0,
-
-  };
-  return Array(6).fill(object);
-};
+apiPlaceholder.getNewReleases = async () => data.items;
 
 apiPlaceholder.getAllLikes = async () => fakeData.likes;
 apiPlaceholder.getLikesFor = async (id) => fakeData.likes.find((l) => l.item_id === id).likes;
