3343d2fadbb6fbc29394f5bfdd7d086ef11aa1f9
brianorora <brianorora@gmail.com>
2021-10-13 22:55:04 +0300

    Create fakeData placeholder

--- src/api/functions.js
+++ src/api/functions.js
@@ -5,30 +5,69 @@ const apiPlaceholder = APIFactory({ credentials, fetch });
 
 // NOTE: THIS are placeholders for the real API
 
+const fakeData = {
+  likes: [
+    {
+      item_id: '5Gso3d4CscCijv0lmajZWd',
+      likes: 0,
+    },
+  ],
+  all_comments: [
+    {
+      item_id: '',
+      comments: [
+        {
+          creation_date: '2021-10-12',
+          username: 'Mrs Man',
+          comment: 'This is the first comment',
+        },
+        {
+          creation_date: '2021-10-13',
+          username: 'Mrs Girl',
+          comment: 'This is the second comment',
+        },
+      ],
+    },
+  ],
+};
+
 apiPlaceholder.getNewReleases = async () => {
   const object = {
     id: '5Gso3d4CscCijv0lmajZWd',
     album_name: 'Life of a DON',
     artist: 'Don Toliver',
     image: 'https://i.scdn.co/image/ab67616d0000b273ab738b25b86bf02f0346c53d',
-    likes: 23,
+    likes: 0,
 
   };
   return Array(6).fill(object);
 };
 
-apiPlaceholder.getAllLikes = async () => [
-  { item_id: 'item_1', likes: 20 },
-  { item_id: 'item_2', likes: 2 },
-  { item_id: 'item_3', likes: 7 },
-];
-apiPlaceholder.getLikesFor = async () => Math.floor(Math.random() * 100);
-apiPlaceholder.getCommentsFor = async () => [
-  { username: 'Mrs Girl', comment: 'This is the first comment', creation_date: '2021-10-12' },
-  { username: 'Mr Man', comment: 'This is the second comment', creation_date: '2021-10-13' },
-];
+apiPlaceholder.getAllLikes = async () => fakeData.likes;
+apiPlaceholder.getLikesFor = async (id) => fakeData.likes.find((l) => l.item_id === id).likes;
+apiPlaceholder.getCommentsFor = async (id) => {
+  const user = fakeData.all_comments.find((c) => c.item_id === id);
+  if (!user) return false;
+  return user.comments;
+};
 
-apiPlaceholder.addCommentFor = async () => true;
-apiPlaceholder.addLikeFor = async () => true;
+apiPlaceholder.addCommentFor = async (id, username, comment) => {
+  const user = fakeData.all_comments.find((c) => c.item_id === id);
+  if (!user) return false;
+  user.comments.push({
+    creation_date: '2021-10-13',
+    username,
+    comment,
+  });
+  return true;
+};
+apiPlaceholder.addLikeFor = async (id) => {
+  const user = fakeData.likes.find((l) => l.item_id === id);
+  if (!user) {
+    return false;
+  }
+  user.likes += 1;
+  return true;
+};
 
 export default apiPlaceholder;
