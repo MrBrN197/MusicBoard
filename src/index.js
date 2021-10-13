dd72714e5cb1a7a2c3ea057930bca39f64438068
Rayan84 <rayan_84@live.com>
2021-10-13 12:56:41 +0200

    Add list contents

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 import './style.scss';
-
 import API from './api/functions.js';
+
+const populateList = (item) => {
+  const itemsContainer = document.getElementById('items-container');
+  const div = document.createElement('DIV');
+  div.setAttribute('class', 'item-container');
+  const photo = document.createElement('IMG');
+  photo.setAttribute('src', item[3]);
+  photo.setAttribute('class', 'album-photo');
+  div.appendChild(photo);
+  const heart = document.createElement('SPAN');
+  heart.innerHTML = '❤';
+  heart.setAttribute('class', 'heart');
+  const title = document.createElement('P');
+  const likes = document.createElement('P');
+  const titleText = document.createTextNode(item[1]);
+  title.appendChild(titleText);
+  title.appendChild(heart);
+  likes.innerHTML = `${item[4]} likes`;
+  const comments = document.createElement('BUTTON');
+  comments.textContent = 'Comments';
+  div.appendChild(title);
+  div.appendChild(likes);
+  div.appendChild(comments);
+  itemsContainer.appendChild(div);
+};
 
 const main = async () => {
   const data = await API.getNewReleases();
-  console.log('Data:', data);
+
+  data.forEach((element) => {
+    const itemDetails = Object.values(element);
+    populateList(itemDetails);
+  });
 };
+
 main();
