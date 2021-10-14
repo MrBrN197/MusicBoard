dd1b66a9bb8d9a6d45caa9f9b7ce12465cdbcd3d
Rayan84 <rayan_84@live.com>
2021-10-14 14:25:19 +0200

    Fix indentation in countTotalItems.test.js

--- src/countTotalItems.test.js
+++ src/countTotalItems.test.js
@@ -5,13 +5,14 @@ import countTotalItems from './countTotalItems.js';
 
 describe('Albums count', () => {
   it('Counts the total number of albums on the page', () => {
-    document.body.innerHTML = `<div>
+    document.body.innerHTML = `
+    <div>
       <div class="item-container">
-     </div>
-    <div class="item-container">
-    </div>
-    <div class="different-container">
-    </div>
+      </div>
+      <div class="item-container">
+      </div>
+      <div class="different-container">
+      </div>
    </div>`;
     expect(countTotalItems()).toBe(2);
   });
