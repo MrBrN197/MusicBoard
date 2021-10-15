280aaf53e70ae78af402ac871fbe5d4b129c6f61
brianorora <brianorora@gmail.com>
2021-10-15 09:54:13 +0300

    Cleanup "src/api/"
    - return `APIFactory(...)` directly from "src/api/functions.js"
    - remove comments from "src/api/functions.js"
    - remove placeholder data from "src/api/data.js"

--- src/api/functions.js
+++ src/api/functions.js
@@ -1,12 +1,4 @@
 import APIFactory from './factory.js';
 import credentials from './credentials.js';
 
-// import data from './data.js';
-
-const apiPlaceholder = APIFactory({ credentials, fetch });
-
-// NOTE: THIS are placeholders for the real API
-
-// apiPlaceholder.getNewReleases = async () => data.items;
-
-export default apiPlaceholder;
+export default APIFactory({ credentials, fetch });
