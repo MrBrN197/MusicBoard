e60d339886b02047d73a4139431aaf30258cac03
brianorora <brianorora@gmail.com>
2021-10-31 23:57:57 +0300

    Enable fake `fetch` in webpack development mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
