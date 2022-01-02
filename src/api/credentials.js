a791445dd42b6644c343f6792b68c60626fc3169
brianorora <brianorora@gmail.com>
2022-01-02 19:47:27 +0300

    Hide client secret and client id

--- src/api/credentials.js
+++ src/api/credentials.js
@@ -1,6 +1,6 @@
 export default {
-  cid: '03c93fbd8ce74b51b2899d8beee8d177',
-  cs: '8342cc936da1483abade1103a1f42d39',
+  cid: process.env.CLIENT_ID,
+  cs: process.env.CLIENT_SECRET,
   token_endpoint: '',
   access_token: '',
 };
